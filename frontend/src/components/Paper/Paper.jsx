278aba0289a463bad57903b3bea4e606b33d087e
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2025-02-12 02:37:54 -0600

    added bolded name

--- frontend/src/components/Paper/Paper.jsx
+++ frontend/src/components/Paper/Paper.jsx
@@ -4,17 +4,24 @@ import "./Paper.css";
 const Paper = ({ title, authors, submission, year, link }) => {
   console.log(submission);
 
-  // Highlight "Oral" by wrapping it in a styled span
+  // Highlight "Oral" in the submission
   const formattedSubmission = submission.replace(
     /(Oral)/gi,
     '<span class="oral-highlight">$1</span>'
+  );
+
+  // Highlight "Yifang Chen" in the authors list
+  const formattedAuthors = authors.replace(
+    /(Yifang Chen)/gi,
+    '<span class="author-highlight">$1</span>'
   );
 
   return (
     <div className="paper">
       <h3>{title}</h3>
       <p>
-        <strong>Authors:</strong> {authors}
+        <strong>Authors:</strong>{" "}
+        <span dangerouslySetInnerHTML={{ __html: formattedAuthors }} />
       </p>
       <p>
         <strong>
