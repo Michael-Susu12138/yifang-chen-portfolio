cde6010c8335839d8c838bb34ec6a331e710691e
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2025-03-07 02:45:45 -0600

    added new article

--- frontend/src/components/PaperList/PaperList.jsx
+++ frontend/src/components/PaperList/PaperList.jsx
@@ -6,6 +6,15 @@ import Paper from "../Paper/Paper"; // Import the Paper component
 const PaperList = () => {
   const papers = [
     {
+      title:
+        "Scaling Law Phenomena Across Regression Paradigms: Multiple and Kernel Approaches",
+      authors:
+        "Yifang Chen, Xuyang Guo, Xiaoyu Li, Yingyu Liang, Zhenmei Shi, Zhao Song.",
+      submission: "arXiv",
+      year: 2025,
+      link: "https://arxiv.org/abs/2503.01314",
+    },
+    {
       title: "Universal Approximation of Visual Autoregressive Transformers",
       authors: "Yifang Chen, Xiaoyu Li, Yingyu Liang, Zhenmei Shi, Zhao Song.",
       submission: "arXiv",
@@ -17,8 +26,8 @@ const PaperList = () => {
         "Fast Gradient Computation for RoPE Attention in Almost Linear Time",
       authors:
         "Yifang Chen, Jiayan Huo, Xiaoyu Li, Yingyu Liang, Zhenmei Shi, Zhao Song.",
-      submission: "arXiv",
-      year: 2024,
+      submission: "ICLR 2025, SCOPE Workshop",
+      year: 2025,
       link: "https://arxiv.org/abs/2412.17316",
     },
   ];
