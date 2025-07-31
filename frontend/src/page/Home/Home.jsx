7e98c38caea1989bbbcee603ff7a53c874ec334b
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2025-07-31 23:04:25 +0800

    test

--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -108,7 +108,7 @@ const Home = () => {
                   fork it
                 </a>{" "}
                 if you'd like to build your own version — and a little credit is
-                always appreciated!
+                always appreciated
               </p>
             </div>
           </section>
